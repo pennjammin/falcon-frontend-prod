fb76d17350125ff9753b551f34cd8d9d2e9714ec
Penn Porterfield <pporterfield@1seo.com>
2018-07-22 23:25:26 -0400

    Put Intercom in the Head

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Link from "next/link";
 import { Config } from "../config.js";
-import Intercom from 'react-intercom';
-
 
 const linkStyle = {
     marginRight: 15
@@ -51,7 +49,6 @@ class Menu extends Component {
             /></a>
         </Link> 
         {menuItems}
-        <Intercom appID="vmn0i5pf"/>
       </div>
     )
   }
